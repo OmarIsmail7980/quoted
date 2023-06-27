0b0abe8e1275d57356128f763d75eb3fad576d68
Omar Ismail <omar.business7980@gmail.com>
2023-06-27 13:15:36 -0500

    editing config

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
 };
-
+console.log(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = initializeFirestore(app, {
